3bb0d9ec4fb5c3e706b81a954946c23d23a513ac
brkdnmz <brkdnmz99@gmail.com>
2018-12-29 17:47:03 +0300

    Password protection added

--- repl_comments.py
+++ repl_comments.py
@@ -1,20 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+from hashlib import sha256
 
-This is a temporary script file.
-"""
+def hashcreator(pw):
+    pw_bytestring = pw.encode()
+    return sha256(pw_bytestring).hexdigest()
 
-i=0
 yorumlar=[]
 numara = 1
+sifre = "378bc297aed78d68f2aa8dbb2064d8beb85f42fab45e1062e1749659a25b0524"
 
-while i==0:
+
+
+while True:
+    kullanici_sifresi = input("Please enter the password to be allowed to comment: ")
+    if str(hashcreator(kullanici_sifresi)) != sifre:
+     while True:
+        print("Sorry but you entered the wrong password :( Please try again.")
+        break
+    else:
+     print("Correct password, WELCOME :) !")
+     break 
+
+        
+    
+        
+
+while True:
     yorum= [input("Please leave a comment: ")]
     yorumlar = yorumlar + yorum
     print("All previous comments of yours: \n")
     for c in yorumlar:
-        print(str(numara) + "- " + str(c) +  "\n")
+        print(str(numara) + "- " + str(c) + "\n")
         numara = numara + 1
     numara=1
     l= input("Leave or stay?(Type 'L' or 'l' to leave, type anything else(or just press enter) to stay): ")
