8541237fb88b646888c62f425969f5129e6595fd
brkdnmz <brkdnmz99@gmail.com>
2018-12-30 23:51:10 +0300

    Latest changes done

--- bottle_app.py
+++ bottle_app.py
@@ -5,7 +5,15 @@
 ### The landing page for assignment 3 should be at /
 #####################################################################
 
-from bottle import route, run, default_app, debug, template, static_file, get
+from bottle import route, run, default_app, debug, template, static_file, get, request, post
+from hashlib import sha256
+
+def hashcreator(pw):
+    pw_bytestring = pw.encode()
+    return sha256(pw_bytestring).hexdigest()
+    
+real_password_hash="378bc297aed78d68f2aa8dbb2064d8beb85f42fab45e1062e1749659a25b0524"
+
 
 def htmlify(title,text):
     page = """
@@ -22,6 +30,9 @@ def htmlify(title,text):
 
     """ % (title,text)
     return page
+
+
+	
     
 @route("/")
 def index():
@@ -34,6 +45,56 @@ def website(filename):
 @route("/static/<filename>")
 def static(filename):
 	return static_file(filename, root="./static")
+	
+login="""
+	<form action="/comments.html" autocomplete="on">
+	 <fieldset>
+	  <legend>You Should Fill Those:</legend>
+	  Name you want to be seen with:
+	  <input type="text" name="nickname" size="36"
+	  placeholder="Like 'crazy_boi', 'ituis18_student', 'ignore_me'..." autofocus required><br><br>
+      The secret password you have to put in there:
+	  <input type="password" name="password" placeholder="A very secret password :) ..." autocomplete="off" required><br><br>
+	  Whose student are you?<br><br>
+	  <input type="radio" name="lecturer" value="djduff">Damien Jade Duff<br>
+	  <input type="radio" name="lecturer" value="turgutuyar">Hayri Turgut Uyar
+	  <br><br>
+	  Are you a boy or a girl?
+	  <select name="gender">
+	   <option value="boy">Boy</option>
+	   <option value="girl">Girl</option>
+	  <br><br>
+	  <input type="image" src="./static/login_icon.gif" title="Login" alt="Login" width=100 height=100>
+	 </fieldset>
+	</form>
+	"""
+	
+@route("/Login")
+def login1():
+	global login
+	return htmlify("Login",login)
+	
+@route("/comments.html")
+def comments():
+	global login
+	if hashcreator(request.GET["password"]) != real_password_hash:
+		if request.GET["gender"] == "boy":
+			login=login+"<br><br><h1>Wrong password :( Please try again, sir!</h1>"
+			return htmlify("Wrong Password", login)
+		elif request.GET["gender"] == "girl":
+			login=login+"<br><br><h1>Wrong password :( Please try again, madam!</h1>"
+			return htmlify("Wrong Password", login)
+	else:
+		write_comment="""
+		Write your comment, please:<br><br>
+		<form>
+		<textarea name="comment" rows="10" cols="30" autofocus></textarea><br>
+		<input type="submit" name="commentsubmit" value="Post">
+		</form>
+		"""
+		return htmlify("Your Lovely Comments",write_comment)
+		
+
 
 
 #####################################################################
